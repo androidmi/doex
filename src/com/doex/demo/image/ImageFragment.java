46ddf7076c5b17ec10c68f51c1194f009d1f8cc3
Du Fan <dufan@dianxinos.com>
2013-03-27 15:52:40 +0800

    image resieze

--- src/com/doex/demo/image/ImageFragment.java
+++ src/com/doex/demo/image/ImageFragment.java
@@ -7,20 +7,23 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.doex.demo.R;
 
-public class ImageFragment extends Fragment implements OnClickListener {
+public class ImageFragment extends Fragment {
 
-    ImageView imgBtn;
-    ImageView big;
-    ImageView middle;
-    ImageView small;
+    private ImageView mLeftImage;
+    private ImageView mRightImage;
+    private ImageView mBottomRightImage;
+    private ImageView mBottomLeftImage;
+
+    String file = "/mnt/sdcard/xxx.png";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,47 +34,36 @@ public class ImageFragment extends Fragment implements OnClickListener {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Activity activity = getActivity();
-        big = (ImageView) activity.findViewById(R.id.big);
-        big.setOnClickListener(this);
-        middle = (ImageView) activity.findViewById(R.id.middle);
-        middle.setOnClickListener(this);
-        small = (ImageView) activity.findViewById(R.id.small);
-        small.setOnClickListener(this);
+        mLeftImage = (ImageView) activity.findViewById(R.id.left);
+        mRightImage = (ImageView) activity.findViewById(R.id.right);
+        mBottomRightImage = (ImageView) activity.findViewById(R.id.bottom_right);
+        mBottomLeftImage = (ImageView) activity.findViewById(R.id.bottom_left);
+        init();
     }
 
-    private Bitmap getBitmpWithFactory(int width, int height) {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        Bitmap m = BitmapFactory.decodeResource(getResources(), R.drawable.badge_1_1_big, options);
-        options.inJustDecodeBounds = false;
-        int be = options.outHeight / height;
-        options.inSampleSize = 3;
+    private void init() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.badge_1_2_middle);
+        Bitmap fileBitmap = BitmapFactory.decodeFile(file);
+        Bitmap map = resizeImage(fileBitmap, bitmap.getWidth(), bitmap.getHeight());
+        mLeftImage.setImageBitmap(map);
 
-        m = BitmapFactory.decodeResource(getResources(), R.drawable.badge_1_1_big, options);
-        // options.inPreferredConfig = Bitmap.Config.RGB_565;
-        // options.inDensity = getResources().getDisplayMetrics().densityDpi;
-        // options.inTargetDensity =
-        // getResources().getDisplayMetrics().densityDpi;
-        // options.outHeight = height;
-        // options.outWidth = width;
-        // options.inSampleSize = 1;
-        // Bitmap newbmp = BitmapFactory.decodeResource(getResources(),
-        // R.drawable.badge_1_1_big,
-        // options);
-        return m;
+        Bitmap leftBottom = Bitmap.createScaledBitmap(fileBitmap, 100,
+                100, true);
+        Bitmap rightBottom = resizeImage(fileBitmap, 100, 100);
+        mBottomLeftImage.setImageBitmap(leftBottom);
+        mBottomRightImage.setImageBitmap(rightBottom);
     }
 
-    private Bitmap getBitmp(Bitmap bitmap, int width, int height) {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        Bitmap newbmp = BitmapFactory.decodeResource(getResources(), R.drawable.badge_1_1_big,
-                options);
-        return newbmp;
-    }
+    /**
+     * the same as Bitmap.createScaledBitmap(fileBitmap, 100, 100, true);
+     * 
+     * @param bitmap
+     * @param w
+     * @param h
+     * @return
+     */
+    private Bitmap resizeImage(Bitmap bitmap, int w, int h) {
 
-    public static Bitmap resizeImage(Bitmap bitmap, int w, int h) {
-
-        // load the origial Bitmap
         Bitmap BitmapOrg = bitmap;
 
         int width = BitmapOrg.getWidth();
@@ -87,39 +79,14 @@ public class ImageFragment extends Fragment implements OnClickListener {
         Matrix matrix = new Matrix();
         // resize the Bitmap
         matrix.postScale(scaleWidth, scaleHeight);
-        // if you want to rotate the Bitmap
         // matrix.postRotate(45);
-        Bitmap.Config config = Bitmap.Config.RGB_565;
 
         // recreate the new Bitmap
         Bitmap resizedBitmap = Bitmap.createBitmap(BitmapOrg, 0, 0, width,
                 height, matrix, true);
 
-        // make a Drawable from Bitmap to allow to set the Bitmap
-        // to the ImageView, ImageButton or what ever
-        // return new BitmapDrawable(resizedBitmap);
         return resizedBitmap;
 
-    }
-
-    @Override
-    public void onClick(View v) {
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.badge_1_1_big);
-        Bitmap bit = null;
-        bit = resizeImage(bitmap, 88, 104);
-        switch (v.getId()) {
-            case R.id.big:
-//                 bit = resizeImage(bitmap, 150, 178);
-                big.setImageBitmap(bit);
-            case R.id.middle:
-//                bit = resizeImage(bitmap, 90, 96);
-                middle.setImageBitmap(bit);
-                break;
-            case R.id.small:
-                bit = resizeImage(bitmap, 45, 45);
-                small.setImageBitmap(bit);
-                break;
-        }
     }
 
 }
